3f9505bde769a75f37511f352d015ab7c5e50f18
dsamaky <darosamaky@gmail.com>
2025-06-06 15:12:16 +0200

    workspace cleaned

--- views/renderMainAppView.js
+++
@@ -1,9 +0,0 @@
-export function renderMainAppView() {
-  return `
-    <div class="app">
-      <h1>Welcome to the Main App</h1>
-      <div id="content"></div>
-      <button id="logout-button">Logout</button>
-    </div>
-  `;
-}
